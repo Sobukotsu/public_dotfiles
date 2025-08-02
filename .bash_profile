bd083cd12b220512e04eba358b05417ed8bcb78c
Sobukotsu <sobukotsu@Sobukotsus-MacBook-Pro.local>
2025-08-03 03:11:22 +0900

    reset

--- .bash_profile
+++
@@ -1,9 +0,0 @@
-#
-# ~/.bash_profile
-#
-
-if [[ -f ~/.bashrc ]]; then
-  . ~/.bashrc
-fi
-
-# [[ -f ~/.bashrc ]] && . ~/.bashrc
--- script/darwin/mactex_exportpath.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-/* TeXLive PATH */
-Year = `date +%Y`;
-export PATH=$PATH:/usr/local/texlive/$Year/bin/universal-darwin
